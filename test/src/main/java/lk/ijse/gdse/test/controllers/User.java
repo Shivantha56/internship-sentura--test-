6d49290f48abd9b5d5d31b4f77439c3977d9a16d
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-02-03 09:23:38 +0530

    dto created

---
+++ test/src/main/java/lk/ijse/gdse/test/controllers/User.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.test.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/user")
+public class User {
+
+    @PostMapping
+    public void save(){
+
+    }
+
+}
---
+++ test/src/main/java/lk/ijse/gdse/test/dto/User.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse.test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    private int uid;
+    private String email;
+    private String given_name;
+    private String middle_name;
+    private String name;
+    private String family_name;
+    private String nickname;
+    private String phone_number;
+    private String comment;
+    private String picture;
+    private Object metadata;
+    private String[] tags;
+    private boolean is_suspend;
+
+
+
+
+}
